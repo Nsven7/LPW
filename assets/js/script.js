90a3cff0c3c4c4a6d230df6815ae35b1cd0ace96
Nathyser <nath_bourlier@outlook.be>
2024-11-09 15:57:58 +0100

    code segmentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,36 @@
 const TAUXINTERET = 0.03;
 let solde = 1000;
-let amount = Number(prompt("Veuillez introduire votre montant").replace(/\D/g, ""));
-let answer = prompt("S'agit-il d'un dépôt ? (tapez \"oui\" ou \"non\") ").trim();
+let depot = 200;
+let retrait = 800;
 let historiqueOperations = [];
 
-while (!amount) {
-    alert("Veuillez introduire un montant valide");
-    amount = Number(prompt("Veuillez introduire votre montant").replace(/\D/g, ""));
-}
-
-while (answer !== "oui" && answer !== "non") {
-    alert("Veuillez introduire une réponse valide");
-    answer = prompt("S'agit-il d'un dépôt ? (tapez \"oui\" ou \"non\") ").trim();
-}
-
-function calculAmount(solde, amount, TAUXINTERET) {
-    if (answer === "oui") {
-        solde += amount;
-        let interest = solde * TAUXINTERET;
-        solde += interest;
-        historiqueOperations.push(`Vous avez déposé : ${amount} euros`);
-        console.log(`Vous avez déposé : ${amount} euros`);
-        console.log(`Intérêts annuels de ${TAUXINTERET}% ajoutés. Montant : ${interest} euros`);
-        console.log(`Nouveau solde : ${solde}`);
-    } else {
-        if (solde >= amount) {
-            solde = solde !== 1000 ? 1000 : solde;
-            solde -= amount;
-            let interest = solde * TAUXINTERET;
-            solde += interest;
-            historiqueOperations.push(`Vous avez retiré ${amount} euros`);
-            console.log(`Intérêts annuels de ${TAUXINTERET}% ajoutés. Montant : ${interest} euros`);
-            console.log(`Vous avez retiré ${amount} euros. Nouveau solde : ${solde}`);
-        } else {
-            console.log("Solde insuffisant pour effectuer ce retrait.");
-        }
-    }
+function addDepot(solde, depot) {
+    solde += depot;
+    historiqueOperations.push(`Vous avez déposé : ${depot} euros`);
+    console.log(`Vous avez déposé : ${depot} euros`);
+    console.log(`Nouveau solde : ${solde}`);
     console.log(historiqueOperations);
-    return solde;
 }
 
-console.log(calculAmount(solde, amount, TAUXINTERET));
+function removeDepot(solde, retrait) {
+    if (solde >= retrait) {
+        solde -= retrait;
+        historiqueOperations.push(`Vous avez retiré ${retrait} euros. Nouveau solde : ${solde}`);
+        console.log(`Vous avez retiré : ${retrait} euros`);
+        console.log(`Nouveau solde : ${solde}`);
+        console.log(historiqueOperations);
+    } else {
+        console.log("Solde insuffisant pour effectuer ce retrait.");
+    }
+}
+
+function calculAmount(solde, TAUXINTERET) {
+    solde += solde * TAUXINTERET;
+    historiqueOperations.push(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${solde}`);
+    console.log((`Intérêts annuels de 3% ajoutés. Nouveau solde : ${solde}`));
+    console.log(historiqueOperations);
+}
+
+addDepot(solde, depot);
+removeDepot(solde, retrait);
+calculAmount(solde, TAUXINTERET);
