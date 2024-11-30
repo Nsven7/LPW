92c8e24e6edff3d43b479c577e54ab7b861d0835
Nathyser <nath_bourlier@outlook.be>
2024-11-30 14:38:24 +0100

    getters and setters added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,25 @@
 import { BankAccount } from '../../src/Controller/BankAccount.js';
 
-let account = new BankAccount(1000);
-account.addDepot(200);
-account.removeDepot(800);
-account.calculAmount();
+let firstSolde = 1000
+let account = new BankAccount(firstSolde)
+
+document.getElementById('numberForm').addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const number = parseInt(document.getElementById('number').value)
+    const operation = document.querySelector('input[name="operation"]:checked').value
+
+    if(operation == "add") {
+        account.addDepot(number);
+    } else {
+        account.removeDepot(number);
+    }
+    
+    account.calculAmount()
+
+    for (let key in account.historiqueOperations) {
+        const operationElement = document.createElement('div');
+        operationElement.textContent = account.historiqueOperations[key];
+        document.getElementById('numberForm').appendChild(operationElement);
+    }
+})
--- src/Controller/BankAccount.js
+++ src/Controller/BankAccount.js
@@ -1,36 +1,88 @@
 class BankAccount {
-    static TAUXINTERET = 0.03;
+    TAUXINTERET = 0.03;
+    #solde
+    #historiqueOperations
 
-    constructor(initialBalance) {
-        this.solde = initialBalance;
-        this.historiqueOperations = [];
+    constructor(solde, historiqueOperations = []) {
+        this.#solde = solde;
+        this.#historiqueOperations = historiqueOperations;
     }
 
-    addDepot(depot) {
-        this.solde += depot;
-        this.historiqueOperations.push(`Vous avez déposé : ${depot} euros`);
-        console.log(`Vous avez déposé : ${depot} euros`);
-        console.log(`Nouveau solde : ${this.solde}`);
-        console.log(this.historiqueOperations);
+    /**
+    * Get the solde of the client.
+    * @return {number} The solde of the client.
+    */
+    get solde() {
+        return this.#solde;
     }
 
-    removeDepot(retrait) {
-        if (this.solde >= retrait) {
-            this.solde -= retrait;
-            this.historiqueOperations.push(`Vous avez retiré ${retrait} euros. Nouveau solde : ${this.solde}`);
-            console.log(`Vous avez retiré : ${retrait} euros`);
-            console.log(`Nouveau solde : ${this.solde}`);
-            console.log(this.historiqueOperations);
+    /**
+     * Set the solde of the client.
+     * @param {solde} solde - The new solde of the client.
+     */
+    set solde(solde) {
+        this.#solde = solde;
+    }
+
+    /**
+    * Get the historiqueOperations of the client.
+    * @return {array} The historiqueOperations of the client.
+    */
+    get historiqueOperations() {
+        return this.#historiqueOperations;
+    }
+
+    /**
+     * Set the historiqueOperations of the client.
+     * @param {array} historiqueOperations - The new historiqueOperations of the client.
+     */
+    set historiqueOperations(historiqueOperations) {
+        this.#historiqueOperations = historiqueOperations;
+    }
+
+    /**
+         * Validate the depot of the client.
+         * @param {number} depot - The depot to validate.
+         * @return {number} The validated depot.
+         * @throws Will throw an error if the depot is not a non-empty string with a maximum size of 20 characters.
+         */
+    #validateDepot(depot) {
+        if (depot <= 0 || typeof depot !== 'number') {
+            throw new Error("The amount must be a float or number");
         } else {
-            console.log("Solde insuffisant pour effectuer ce retrait.");
+            return depot;
         }
     }
 
+    /**
+         * Validate the depot of the client.
+         * @param {number} depot - The depot to validate.
+         * @return {number} The validated depot.
+         * @throws Will throw an error if the depot is not a non-empty string with a maximum size of 20 characters.
+         */
+    #removeDepot(depot, solde) {
+        if (depot > this.#solde || depot <= 0 || typeof depot !== 'number') {
+            throw new Error("The amount can't be superior to your solde");
+        } else {
+            return depot;
+        }
+    }
+
+    addDepot(depot) {
+        this.#solde += this.#validateDepot(depot);
+        this.#historiqueOperations.push(`Vous avez déposé : ${depot} €.`);
+    }
+
+    removeDepot(retrait) {
+        this.#solde -= this.#removeDepot(retrait);
+        this.#historiqueOperations.push(`Vous avez retiré ${retrait}€.`);
+        this.#historiqueOperations.push(`Nouveau solde : ${this.#solde}€.`);
+    }
+
     calculAmount() {
-        this.solde += this.solde * BankAccount.TAUXINTERET;
-        this.historiqueOperations.push(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${this.solde}`);
-        console.log(`Intérêts annuels de 3% ajoutés. Nouveau solde : ${this.solde}`);
-        console.log(this.historiqueOperations);
+        this.#solde += this.#solde * this.TAUXINTERET;
+        this.#historiqueOperations.push(`Intérêts annuels de 3% ajoutés.`);
+        this.#historiqueOperations.push(`Nouveau solde : ${this.#solde}€.`);
     }
 }
 
